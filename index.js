2b2d7111838af589fb725113c7cc0c6e1c0003fb
Waseth <waseth.emmanuel@student.moringaschool.com>
2024-04-11 21:13:01 +0300

    added extra comments to my index.js code

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-// I targeted all elements with a tag of "button".
+// I targeted and selected all elements with a tag of "button".
 const buttonElements = document.querySelectorAll("button")
+//I targeted and selected the input field.
 const inputField = document.getElementById("res")
 //I created a for loop that iterates over the buttonElements until the specified condition becomes false.
 for(let x =0;x<buttonElements.length; x++){
 //I added an Eventlistener, "click" to each button.
     buttonElements[x].addEventListener("click" ,()=>{
        const val = buttonElements[x].textContent;
+//If val is "CLEAR",pass it to the callback function of deleteResults.
        if(val ==="CLEAR"){
          deleteResults()
+//Else if val is "=",pass it to the callback function of finalAnswer.
        }else if(val === "="){
           finalAnswer()
-       }else{
+//Else pass to the addVal(val) callback function.
+       }else {
           addVal(val);
        }
+
+
     });
 }
+
 function deleteResults(){
     inputField.value ="";
 }
@@ -25,3 +32,4 @@ function addVal(val){
     inputField.value += val;
 
 }
+
